659edaef3f2206885044bd58c50786f284a7a59e
Sreejith s <imsreejithofficial@gmail.com>
2021-08-11 18:08:05 +0530

    set entry point

--- main.py
+++ main.py
@@ -1,72 +1,14 @@
+"""entry point"""
+
 from fastapi import FastAPI, Depends
-from pydantic import BaseModel
-from typing import Optional, List
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker, Session
-from sqlalchemy import Boolean, Column, Float, String, Integer
 
 app = FastAPI()
 
-# SqlAlchemy Setup
-SQLALCHEMY_DATABASE_URL = 'sqlite+pysqlite:///./db.sqlite3:'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# A SQLAlchemny ORM Place
-class DBPlace(Base):
-    __tablename__ = 'places'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50))
-    description = Column(String, nullable=True)
-    coffee = Column(Boolean)
-    wifi = Column(Boolean)
-    food = Column(Boolean)
-    lat = Column(Float)
-    lng = Column(Float)
-
-Base.metadata.create_all(bind=engine)
-
-# A Pydantic Place
-class Place(BaseModel):
-    name: str
-    description: Optional[str] = None
-    coffee: bool
-    wifi: bool
-    food: bool
-    lat: float
-    lng: float
-
-    class Config:
-        orm_mode = True
-
-# Methods for interacting with the database
-def get_place(db: Session, place_id: int):
-    return db.query(DBPlace).where(DBPlace.id == place_id).first()
-
-def get_places(db: Session):
-    return db.query(DBPlace).all()
-
-def create_place(db: Session, place: Place):
-    db_place = DBPlace(**place.dict())
-    db.add(db_place)
-    db.commit()
-    db.refresh(db_place)
-
-    return db_place
 
 # Routes for interacting with the API
-@app.post('/places/', response_model=Place)
-def create_places_view(place: Place, db: Session = Depends(get_db)):
-    db_place = create_place(db, place)
+@app.post('/user/', response_model=User)
+def CreateUser(user: User, db: Session = Depends(get_db)):
+     = create_place(db, place)
     return db_place
 
 @app.get('/places/', response_model=List[Place])
